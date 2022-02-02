71d4607f01c5be15a2baab762856ddc1c586997f
Eoghan <eoghan.fletcher@outlook.com>
2022-02-02 21:08:52 +0000

    Added Client-side and Middleware projects

---
+++ MiddlewareDiscussionManagement/src/main/java/com/middleware/middlewarediscussionmanagement/MiddlewareDiscussionManagementApplication.java
@@ -0,0 +1,13 @@
+package com.middleware.middlewarediscussionmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewareDiscussionManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddlewareDiscussionManagementApplication.class, args);
+    }
+
+}
---
+++ MiddlewareDiscussionManagement/src/test/java/com/middleware/middlewarediscussionmanagement/MiddlewareDiscussionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.middleware.middlewarediscussionmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddlewareDiscussionManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
